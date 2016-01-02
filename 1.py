938c4517657ab6027ca49b68bc35051ae1ddf12f
alex <jiahangsocket@163.com>
2016-01-02 12:13:35 +0800

    指定项目编码

--- 1.py
+++ 1.py
@@ -14,6 +14,6 @@ def commit(msg):
     os.system('git commit -m %s' % (msg))
 
 if __name__ == '__main__':
-    set_sys_date(2018,5,2)   #设置那一天
+    set_sys_date(2016,1,2)   #设置那一天
     set_sys_time()
-    commit("武汉通")           #想要深色点，最低三个提交
+    commit("指定项目编码")               #想要深色点，最低三个提交
