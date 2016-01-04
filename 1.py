f02acd4f5430a806b409a06545630ebc4870deaa
alex <jiahangsocket@163.com>
2016-01-04 10:12:04 +0800

    注解

--- 1.py
+++ 1.py
@@ -16,4 +16,4 @@ def commit(msg):
 if __name__ == '__main__':
     set_sys_date(2016,1,4)   #设置那一天
     set_sys_time()
-    commit("rebase和merge")               #想要深色点，最低三个提交
+    commit("注解")               #想要深色点，最低三个提交
