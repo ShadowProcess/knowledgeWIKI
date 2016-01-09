aff69bdc3474c6f90b57e9095c941d8d6a196c1b
alex <jiahangsocket@163.com>
2016-01-09 23:19:06 +0800

    java元注解

--- 1.py
+++ 1.py
@@ -14,6 +14,6 @@ def commit(msg):
     os.system('git commit -m %s' % (msg))
 
 if __name__ == '__main__':
-    set_sys_date(2016,1,8)   #设置那一天
+    set_sys_date(2016,1,9)   #设置那一天
     set_sys_time()
-    commit("value注解")               #想要深色点，最低三个提交
+    commit("java元注解")               #想要深色点，最低三个提交
