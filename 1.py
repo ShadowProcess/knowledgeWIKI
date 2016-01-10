0a6dce0a00175b7fe2ae08fe52ffac487e358f02
alex <jiahangsocket@163.com>
2016-01-10 10:01:38 +0800

    海明校验码

--- 1.py
+++ 1.py
@@ -7,13 +7,13 @@ def set_sys_date(year, month, day):
     os.system('date %04d/%02d/%02d' % (year, month, day))
 
 def set_sys_time():
-    os.system('time %02d:%02d:%02d' % (random.randint(8,23), random.randint(0,60),random.randint(0,60)))
+    os.system('time %02d:%02d:%02d' % (random.randint(8,23), random.randint(0,59),random.randint(0,59)))
 
 def commit(msg):
     os.system('git add .')
     os.system('git commit -m %s' % (msg))
 
 if __name__ == '__main__':
-    set_sys_date(2016,1,9)   #设置那一天
+    set_sys_date(2016,1,10)   #设置那一天
     set_sys_time()
-    commit("java元注解")               #想要深色点，最低三个提交
+    commit("海明校验码")               #想要深色点，最低三个提交
