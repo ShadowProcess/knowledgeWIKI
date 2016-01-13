2c2c2ff9efc9c4f89c43862cc3b86bc7d29c9af8
alex <jiahangsocket@163.com>
2016-01-13 21:55:49 +0800

    api

--- 1.py
+++ 1.py
@@ -14,6 +14,6 @@ def commit(msg):
     os.system('git commit -m %s' % (msg))
 
 if __name__ == '__main__':
-    set_sys_date(2016,1,10)   #设置那一天
+    set_sys_date(2016,1,13)   #设置那一天
     set_sys_time()
-    commit("海明校验码")               #想要深色点，最低三个提交
+    commit("api")               #想要深色点，最低三个提交
