ffa1ffcd55e0f7804ab82170acc44d0b166e250e
alex <jiahangsocket@163.com>
2016-01-19 16:19:38 +0800

    源码

--- 1.py
+++ 1.py
@@ -14,6 +14,6 @@ def commit(msg):
     os.system('git commit -m %s' % (msg))
 
 if __name__ == '__main__':
-    set_sys_date(2016,1,13)   #设置那一天
+    set_sys_date(2016,1,19)   #设置那一天
     set_sys_time()
-    commit("多级指针")               #想要深色点，最低三个提交
+    commit("源码")               #想要深色点，最低三个提交
