3fe9f08bb8560de76162b19e68d4318dc9a01357
alex <jiahangsocket@163.com>
2016-02-14 09:41:22 +0800

    指针热身

--- 1.py
+++ 1.py
@@ -14,6 +14,6 @@ def commit(msg):
     os.system('git commit -m %s' % (msg))
 
 if __name__ == '__main__':
-    set_sys_date(2016,2,2)   #设置那一天
+    set_sys_date(2016,2,14)   #设置那一天
     set_sys_time()
-    commit("指针")               #想要深色点，最低三个提交
+    commit("指针热身")               #想要深色点，最低三个提交
