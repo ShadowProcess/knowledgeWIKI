9578388371bc7f73ddfedb90f8916bbd5d616b43
alex <jiahangsocket@163.com>
2016-03-08 19:46:59 +0800

    c复习

--- 1.py
+++ 1.py
@@ -14,6 +14,6 @@ def commit(msg):
     os.system('git commit -m %s' % (msg))
 
 if __name__ == '__main__':
-    set_sys_date(2016,3,1)   #设置那一天
+    set_sys_date(2016,3,8)   #设置那一天
     set_sys_time()
-    commit("函数声明")               #想要深色点，最低三个提交
+    commit("c复习")               #想要深色点，最低三个提交
