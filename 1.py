bb891d5651b23db3a67b6218d9c0f1906f633403
alex <jiahangsocket@163.com>
2016-03-28 10:15:22 +0800

    hash算法

--- 1.py
+++ 1.py
@@ -14,6 +14,6 @@ def commit(msg):
     os.system('git commit -m %s' % (msg))
 
 if __name__ == '__main__':
-    set_sys_date(2016,3,19)   #设置那一天
+    set_sys_date(2016,3,28)         #设置那一天
     set_sys_time()
-    commit("dos攻击")               #想要深色点，最低三个提交
+    commit("hash算法")               #想要深色点，最低三个提交
