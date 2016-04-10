7359c867c8cba8b9f4c3e95ce3061c96cfc41dc6
alex <jiahangsocket@163.com>
2016-04-10 11:38:26 +0800

    图

--- 1.py
+++ 1.py
@@ -14,6 +14,6 @@ def commit(msg):
     os.system('git commit -m %s' % (msg))
 
 if __name__ == '__main__':
-    set_sys_date(2016,4,7)         #设置那一天
+    set_sys_date(2016,4,10)         #设置那一天
     set_sys_time()
-    commit("http协议")               #想要深色点，最低三个提交
+    commit("图")               #想要深色点，最低三个提交
