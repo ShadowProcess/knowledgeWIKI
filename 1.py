88ae3927a368ca8c9d04a57d39b56be05383dcbd
alex <jiahangsocket@163.com>
2016-06-01 21:50:45 +0800

    intern方法

--- 1.py
+++ 1.py
@@ -14,6 +14,6 @@ def commit(msg):
     os.system('git commit -m %s' % (msg))
 
 if __name__ == '__main__':
-    set_sys_date(2016,5,28)         #设置那一天
+    set_sys_date(2016,6,1)         #设置那一天
     set_sys_time()
-    commit("jvm")               #想要深色点，最低三个提交
+    commit("intern方法")               #想要深色点，最低三个提交
