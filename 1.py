177903c3f90e908e0ec8c298e79f0d9496583e45
alex <jiahangsocket@163.com>
2016-08-22 16:19:25 +0800

    密码学

--- 1.py
+++ 1.py
@@ -14,6 +14,6 @@ def commit(msg):
     os.system('git commit -m %s' % (msg))
 
 if __name__ == '__main__':
-    set_sys_date(2016,8,5)         #设置那一天
+    set_sys_date(2016,8,22)         #设置那一天
     set_sys_time()
-    commit("==")               #想要深色点，最低三个提交
+    commit("密码学")               #想要深色点，最低三个提交
