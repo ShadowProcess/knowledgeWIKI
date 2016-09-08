104f47afbf39720cc883f2307ed33b5398cb3e03
alex <jiahangsocket@163.com>
2016-09-08 14:40:17 +0800

    命令

--- 1.py
+++ 1.py
@@ -14,6 +14,6 @@ def commit(msg):
     os.system('git commit -m %s' % (msg))
 
 if __name__ == '__main__':
-    set_sys_date(2016,9,7)         #设置那一天
+    set_sys_date(2016,9,8)         #设置那一天
     set_sys_time()
-    commit("重要命令集锦")               #想要深色点，最低三个提交
+    commit("命令")               #想要深色点，最低三个提交
