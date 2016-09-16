eb8c2b32a1f6f0ad81b56f912a981d063a6d0c81
alex <jiahangsocket@163.com>
2016-09-16 23:18:18 +0800

    tree

--- 1.py
+++ 1.py
@@ -14,6 +14,6 @@ def commit(msg):
     os.system('git commit -m %s' % (msg))
 
 if __name__ == '__main__':
-    set_sys_date(2016,9,12)         #设置那一天
+    set_sys_date(2016,9,16)         #设置那一天
     set_sys_time()
-    commit("kill")               #想要深色点，最低三个提交
+    commit("tree")               #想要深色点，最低三个提交
