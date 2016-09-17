e7ce6d5650daf61d09796dc8e3a049b3f4a1bf71
alex <jiahangsocket@163.com>
2016-09-17 13:07:21 +0800

    软件安装

--- 1.py
+++ 1.py
@@ -14,6 +14,6 @@ def commit(msg):
     os.system('git commit -m %s' % (msg))
 
 if __name__ == '__main__':
-    set_sys_date(2016,9,16)         #设置那一天
+    set_sys_date(2016,9,17)         #设置那一天
     set_sys_time()
-    commit("tree")               #想要深色点，最低三个提交
+    commit("软件安装")               #想要深色点，最低三个提交
