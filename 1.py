6a18bf1fc2b80817c620981f63cafc67e941e002
alex <jiahangsocket@163.com>
2016-09-26 14:48:05 +0800

    mysql安装

--- 1.py
+++ 1.py
@@ -14,6 +14,6 @@ def commit(msg):
     os.system('git commit -m %s' % (msg))
 
 if __name__ == '__main__':
-    set_sys_date(2016,9,17)         #设置那一天
+    set_sys_date(2016,9,26)         #设置那一天
     set_sys_time()
-    commit("图形协议")               #想要深色点，最低三个提交
+    commit("mysql安装")               #想要深色点，最低三个提交
