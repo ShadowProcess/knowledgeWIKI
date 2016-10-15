04a9371a9dfc516d0f16f45a2e055382f6afec02
alex <jiahangsocket@163.com>
2016-10-15 11:35:50 +0800

    mac地址

--- 1.py
+++ 1.py
@@ -14,6 +14,6 @@ def commit(msg):
     os.system('git commit -m %s' % (msg))
 
 if __name__ == '__main__':
-    set_sys_date(2016,10,13)         #设置那一天
+    set_sys_date(2016,10,15)         #设置那一天
     set_sys_time()
-    commit("shell")               #想要深色点，最低三个提交
+    commit("mac地址")               #想要深色点，最低三个提交
