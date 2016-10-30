20ca452210988fc7e9f67fb6ca1b07db278b087a
alex <jiahangsocket@163.com>
2016-10-30 14:12:32 +0800

    nginx-1

--- 1.py
+++ 1.py
@@ -14,6 +14,6 @@ def commit(msg):
     os.system('git commit -m %s' % (msg))
 
 if __name__ == '__main__':
-    set_sys_date(2016,10,22)         #设置那一天
+    set_sys_date(2016,10,30)         #设置那一天
     set_sys_time()
     commit("nginx-1")               #想要深色点，最低三个提交
