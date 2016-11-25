fe4b964a485541b37a7830aa2fbf35037905cfe7
alex <jiahangsocket@163.com>
2016-11-25 17:35:46 +0800

    ssl

--- 1.py
+++ 1.py
@@ -14,6 +14,6 @@ def commit(msg):
     os.system('git commit -m %s' % (msg))
 
 if __name__ == '__main__':
-    set_sys_date(2016,11,11)         #设置那一天
+    set_sys_date(2016,11,25)         #设置那一天
     set_sys_time()
-    commit("spring")               #想要深色点，最低三个提交
+    commit("ssl")               #想要深色点，最低三个提交
