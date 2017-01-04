befcb186b5f0bf883db5327c1703cd6c4bab6c63
alex <jiahangsocket@163.com>
2017-01-04 11:45:54 +0800

    spring常见事务

--- 1.py
+++ 1.py
@@ -16,4 +16,4 @@ def commit(msg):
 if __name__ == '__main__':
     set_sys_date(2017,1,4)         #设置那一天
     set_sys_time()
-    commit("spring事务")               #想要深色点，最低三个提交
+    commit("spring常见事务")               #想要深色点，最低三个提交
