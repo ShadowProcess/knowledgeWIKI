21a5612363e4d68956d596fa7619dd0750d9a79d
alex <jiahangsocket@163.com>
2017-02-20 11:15:59 +0800

    java反射

--- 1.py
+++ 1.py
@@ -14,6 +14,6 @@ def commit(msg):
     os.system('git commit -m %s' % (msg))
 
 if __name__ == '__main__':
-    set_sys_date(2017,2,16)         #设置那一天
+    set_sys_date(2017,2,20)         #设置那一天
     set_sys_time()
-    commit("cglib")               #想要深色点，最低三个提交
+    commit("java反射")               #想要深色点，最低三个提交
