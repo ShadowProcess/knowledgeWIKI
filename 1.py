c42112a5db331b402c2234695c3ed2d79eaeb56f
alex <jiahangsocket@163.com>
2017-03-23 22:20:29 +0800

    meta-info

--- 1.py
+++ 1.py
@@ -14,6 +14,6 @@ def commit(msg):
     os.system('git commit -m %s' % (msg))
 
 if __name__ == '__main__':
-    set_sys_date(2017,3,19)         #设置那一天
+    set_sys_date(2017,3,23)         #设置那一天
     set_sys_time()
-    commit("3.0概念")               #想要深色点，最低三个提交
+    commit("meta-info")               #想要深色点，最低三个提交
