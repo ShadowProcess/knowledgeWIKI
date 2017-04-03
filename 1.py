3d18edcf361549815ddc40199ffea43c708d97e9
alex <jiahangsocket@163.com>
2017-04-03 23:57:43 +0800

    缓存问题

--- 1.py
+++ 1.py
@@ -14,6 +14,6 @@ def commit(msg):
     os.system('git commit -m %s' % (msg))
 
 if __name__ == '__main__':
-    set_sys_date(2017,3,28)         #设置那一天
+    set_sys_date(2017,4,3)         #设置那一天
     set_sys_time()
-    commit("扩展")               #想要深色点，最低三个提交
+    commit("缓存问题")               #想要深色点，最低三个提交
