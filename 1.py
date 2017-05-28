28a2c56a781b853a14b872e67f0100b5dc0408c7
alex <jiahangsocket@163.com>
2017-05-28 14:00:24 +0800

    函数表达式

--- 1.py
+++ 1.py
@@ -28,6 +28,6 @@ def commit(msg):
 
 
 if __name__ == '__main__':
-    set_sys_date(2017,5,25)         #设置那一天
+    set_sys_date(2017,5,28)         #设置那一天
     set_sys_time()
-    commit("生成字节码")               #想要深色点，最低三个提交
+    commit("函数表达式")               #想要深色点，最低三个提交
