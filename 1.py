fa82be39b899a75c9801fbf69007f3687cea537a
alex <jiahangsocket@163.com>
2017-08-20 14:46:55 +0800

    字符串操作2

--- 1.py
+++ 1.py
@@ -26,6 +26,6 @@ def commit(msg):
 
 
 if __name__ == '__main__':
-    set_sys_date(2017,8,20)         #设置那一天
+    set_sys_date(2017,8,20)          #设置那一天
     set_sys_time()
-    commit("字符串操作1")               #想要深色点，最低三个提交
+    commit("字符串操作2")               #想要深色点，最低三个提交
