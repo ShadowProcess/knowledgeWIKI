ea63d20c8e255fe7283043a160900b69a6e8d16d
alex <jiahangsocket@163.com>
2017-10-10 22:13:53 +0800

    相关jar

--- 1.py
+++ 1.py
@@ -26,6 +26,6 @@ def commit(msg):
 
 
 if __name__ == '__main__':
-    set_sys_date(2017,10,7)          #设置那一天
+    set_sys_date(2017,10,10)          #设置那一天
     set_sys_time()
-    commit("加密")                    #想要深色点，最低三个提交
+    commit("相关jar")                    #想要深色点，最低三个提交
