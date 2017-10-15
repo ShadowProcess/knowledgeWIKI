1b349c0548387be59f6fea0f25641069f00bd42d
alex <jiahangsocket@163.com>
2017-10-15 21:50:44 +0800

    加密工具1

--- 1.py
+++ 1.py
@@ -28,4 +28,4 @@ def commit(msg):
 if __name__ == '__main__':
     set_sys_date(2017,10,15)          #设置那一天
     set_sys_time()
-    commit("加密工具")                    #想要深色点，最低三个提交
+    commit("加密工具1")                    #想要深色点，最低三个提交
