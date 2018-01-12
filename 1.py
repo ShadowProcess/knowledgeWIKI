5b2cce7bacea74f7370c1c51a65e52d96918a0d0
alex <jiahangsocket@163.com>
2018-01-12 15:12:02 +0800

    全局参数

--- 1.py
+++ 1.py
@@ -25,4 +25,4 @@ def commit(msg):
 if __name__ == '__main__':
     set_sys_date(2018,1,12)                 #设置那一天
     set_sys_time()
-    commit("原生")                    #想要深色点，最低三个提交
+    commit("全局参数")                    #想要深色点，最低三个提交
