31849744fb148751d5016c281eaadb86ff35c01b
alex <jiahangsocket@163.com>
2018-02-07 22:09:41 +0800

    docker

--- 1.py
+++ 1.py
@@ -23,6 +23,6 @@ def commit(msg):
 
 
 if __name__ == '__main__':
-    set_sys_date(2018,1,28)                 #设置那一天
+    set_sys_date(2018,2,7)                 #设置那一天
     set_sys_time()
-    commit("连接池")                    #想要深色点，最低三个提交
+    commit("docker")                    #想要深色点，最低三个提交
