977ab29edb71cd39154f9b3be2761f20903ac1c5
alex <jiahangsocket@163.com>
2018-02-08 12:24:12 +0800

    docker概念

--- 1.py
+++ 1.py
@@ -23,6 +23,6 @@ def commit(msg):
 
 
 if __name__ == '__main__':
-    set_sys_date(2018,2,7)                 #设置那一天
+    set_sys_date(2018,2,8)                 #设置那一天
     set_sys_time()
-    commit("docker")                    #想要深色点，最低三个提交
+    commit("docker概念")                    #想要深色点，最低三个提交
