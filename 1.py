73e512f566d07a7e722f8387d9e92fc795c79d94
alex <jiahangsocket@163.com>
2018-06-15 23:11:46 +0800

    spring4

--- 1.py
+++ 1.py
@@ -23,6 +23,6 @@ def commit(msg):
 
 
 if __name__ == '__main__':
-    set_sys_date(2018,6,10)                 #设置那一天
+    set_sys_date(2018,6,15)                 #设置那一天
     set_sys_time()
-    commit("zset")                         #想要深色点，最低三个提交
+    commit("spring4")                         #想要深色点，最低三个提交
