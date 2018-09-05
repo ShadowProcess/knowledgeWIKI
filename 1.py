6a5688ed6f21a966812114ce8f7fffcd47f943c1
alex <jiahangsocket@163.com>
2018-09-05 13:26:14 +0800

    注解资源

--- 1.py
+++ 1.py
@@ -23,6 +23,6 @@ def commit(msg):
 
 
 if __name__ == '__main__':
-    set_sys_date(2018,8,25)                 #设置那一天
+    set_sys_date(2018,9,5)                 #设置那一天
     set_sys_time()
-    commit("spring4注解-2")                         #想要深色点，最低三个提交
+    commit("注解资源")                         #想要深色点，最低三个提交
