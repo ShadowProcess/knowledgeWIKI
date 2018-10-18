5eccdec6c44ea8299077743cb45d4a30522da14c
alex <jiahangsocket@163.com>
2018-10-18 11:51:28 +0800

    spring5-注解-bean

---
+++ spring5-annotation/src/main/java/com/example/bean/User.java
@@ -0,0 +1,35 @@
+package com.example.bean;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component(value = "user")
+public class User {
+    private Integer id;
+    private String name;
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
