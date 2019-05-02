dd1592d05ee8e13112b7380a9b63b5267b379413
alex <jiahangsocket@163.com>
2019-05-02 10:32:02 +0800

    数据库配置

--- 1.py
+++ 1.py
@@ -23,7 +23,7 @@ def commit(msg):
 
 
 if __name__ == '__main__':
-    set_sys_date(2019,5,1)
+    set_sys_date(2019,5,2)
     #set_sys_date(2019,5,8)
     #set_sys_date(2019,5,10)
     #set_sys_date(2019,7,24)
@@ -79,4 +79,4 @@ if __name__ == '__main__':
     #set_sys_date(2020,7,10)
     #set_ys_date(2020,7,15)
     set_sys_time()
-    commit("pro")                         #想要深色点，最低三个提交
+    commit("数据库配置")                         #想要深色点，最低三个提交
