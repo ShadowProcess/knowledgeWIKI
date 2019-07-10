675a33c0978c4e3236ac47ea475abf6560ed53d5
alex <jiahangsocket@163.com>
2019-07-10 23:53:53 +0800

    mybatis

--- 1.py
+++ 1.py
@@ -23,6 +23,6 @@ def commit(msg):
 
 
 if __name__ == '__main__':
-    set_sys_date(2019,7,7)
+    set_sys_date(2019,7,10)
     set_sys_time()
-    commit("3.0规范")                         #想要深色点，最低三个提交
+    commit("mybatis")                         #想要深色点，最低三个提交
