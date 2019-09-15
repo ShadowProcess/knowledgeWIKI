a63189c86f2d4dc5952233cbba2f965c46d4e9b8
alex <jiahangsocket@163.com>
2019-09-15 17:55:29 +0800

    准备

--- 1.py
+++ 1.py
@@ -23,6 +23,6 @@ def commit(msg):
 
 
 if __name__ == '__main__':
-    set_sys_date(2019,9,11)
+    set_sys_date(2019,9,15)
     set_sys_time()
-    commit("建设")                         #想要深色点，最低三个提交
+    commit("准备")                         #想要深色点，最低三个提交
