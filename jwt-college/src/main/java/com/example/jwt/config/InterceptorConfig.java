9ca07c874eaffaaad0f97227b70ac7f625446594
alex <jiahangsocket@163.com>
2019-12-22 18:02:35 +0800

    撒大声地

---
+++ jwt-college/src/main/java/com/example/jwt/config/InterceptorConfig.java
@@ -0,0 +1,21 @@
+package com.example.jwt.config;
+
+import com.example.jwt.Interceptor.AuthenticationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor()).addPathPatterns("/**");
+    }
+
+    @Bean
+    public AuthenticationInterceptor interceptor() {
+        return new AuthenticationInterceptor();
+    }
+}
