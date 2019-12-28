ea9c4d3a9a6e58ebebd0b9e91c5d103a77d541a4
alex <jiahangsocket@163.com>
2019-12-28 15:08:03 +0800

    常量

--- 1.py
+++ 1.py
@@ -23,6 +23,6 @@ def commit(msg):
 
 
 if __name__ == '__main__':
-    set_sys_date(2019,12,22)
+    set_sys_date(2019,12,28)
     set_sys_time()
-    commit("撒大声地")                         #想要深色点，最低三个提交
+    commit("常量")                         #想要深色点，最低三个提交
---
+++ jwt-college/src/main/java/com/example/jwt/constant/ConstantKey.java
@@ -0,0 +1,5 @@
+package com.example.jwt.constant;
+
+public class ConstantKey {
+    public static final String PICEA_JWT_KEY = "spring-boot-jwt-key~#^";
+}
