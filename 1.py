4fa6c9156a1a7d08dff0796edbb88b83e4d25eff
alex <jiahangsocket@163.com>
2020-02-16 08:03:16 +0800

    导入

--- 1.py
+++ 1.py
@@ -57,4 +57,4 @@ if __name__ == '__main__':
     #set_sys_date(2020,7,10)
     #set_ys_date(2020,7,15)
     set_sys_time()
-    commit("对接vue")                         #想要深色点，最低三个提交
+    commit("导入")                         #想要深色点，最低三个提交
