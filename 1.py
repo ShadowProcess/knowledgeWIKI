2978e7311190db5c2b797d30664e95cc7948225a
alex <jiahangsocket@163.com>
2020-04-08 22:31:12 +0800

    页面

--- 1.py
+++ 1.py
@@ -39,4 +39,4 @@ if __name__ == '__main__':
     #set_sys_date(2020,7,10)
     #set_ys_date(2020,7,15)
     set_sys_time()
-    commit("tk")                         #想要深色点，最低三个提交
+    commit("页面")                         #想要深色点，最低三个提交
