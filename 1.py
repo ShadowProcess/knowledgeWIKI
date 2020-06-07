22da687e5fd8b0bc88b2e1876899aab40aa916f3
alex <jiahangsocket@163.com>
2020-06-07 10:52:32 +0800

    fx()

--- 1.py
+++ 1.py
@@ -19,8 +19,7 @@ def commit(msg):
 
 
 if __name__ == '__main__':
-    set_sys_date(2020,5,29)
-    #set_sys_date(2020,6,7)
+    set_sys_date(2020,6,7)
     #set_sys_date(2020,6,13)
     #set_sys_date(2020,6,17)
     #set_sys_date(2020,6,21)
@@ -30,4 +29,4 @@ if __name__ == '__main__':
     #set_sys_date(2020,7,10)
     #set_ys_date(2020,7,15)
     set_sys_time()
-    commit("token")                         #想要深色点，最低三个提交
+    commit("fx()")                         #想要深色点，最低三个提交
