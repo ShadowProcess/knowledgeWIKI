3905bb2dcead92ec3065239cbfff10920319b7ce
alex <jiahangsocket@163.com>
2020-07-30 19:54:08 +0800

    1.8可重复注解

---
+++ java-8/src/main/java/com/example/java/day02/annotation/MyAnnotation.java
@@ -0,0 +1,15 @@
+package com.example.java.day02.annotation;
+
+import java.lang.annotation.*;
+
+import static java.lang.annotation.ElementType.*;
+
+//如想使用重复注解 必须用这个注解@Repeatable修饰一下 参数为注解容器
+@Repeatable(MyAnnotations.class)
+@Target({TYPE, FIELD, METHOD, PARAMETER, CONSTRUCTOR, LOCAL_VARIABLE, TYPE_PARAMETER}) //TYPE_PARAMETER这个是新的，可以修饰参数
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MyAnnotation {
+
+    String value() default "xxx";
+
+}
