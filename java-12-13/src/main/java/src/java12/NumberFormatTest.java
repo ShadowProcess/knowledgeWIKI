7a4ba6a72f936283509261d25ed94437786f39a9
alex <jiahangsocket@163.com>
2020-08-04 22:43:19 +0800

    java12-13

---
+++ java-12-13/src/main/java/src/java12/NumberFormatTest.java
@@ -0,0 +1,21 @@
+package src.java12;
+
+import org.junit.Test;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+
+public class NumberFormatTest {
+    //支持压缩数字格式化
+    @Test
+    public void testCompactNumberFormat(){
+        var cnf = NumberFormat.getCompactNumberInstance(Locale.US, NumberFormat.Style.SHORT);
+        System.out.println(cnf.format(1_0000));
+        System.out.println(cnf.format(1_9200));
+        System.out.println(cnf.format(1_000_000));
+        System.out.println(cnf.format(1L << 30));
+        System.out.println(cnf.format(1L << 40));
+        System.out.println(cnf.format(1L << 50));
+    }
+}
---
+++ java-12-13/src/main/java/src/java12/StringConstantTest.java
@@ -0,0 +1,18 @@
+package src.java12;
+
+import java.util.Optional;
+
+
+public class StringConstantTest {
+
+    private static void testDescribeConstable() {
+        System.out.println("======test java 12 describeConstable======");
+        String name = "尚硅谷Java高级工程师";
+        Optional<String> optional = name.describeConstable();
+        System.out.println(optional.get());
+    }
+
+    public static void main(String[] args) {
+        testDescribeConstable();
+    }
+}
