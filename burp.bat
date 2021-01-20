3a33e91959de391a2e2168ea7bc8c92b4487f591
alex <jiahangsocket@163.com>
2021-01-20 12:15:06 +0800

    1.Mysql explain retrospect

---
+++ burp.bat
@@ -0,0 +1,17 @@
+REM 声明采用UTF-8编码(注释)
+chcp 65001
+
+
+cd \
+echo "已切换至C盘根目录"
+pause
+
+
+D:
+echo "已切换至D盘"
+pause
+
+
+cd D:\DevelopmentSoftWare\Burp_Suite_Pro_v1.7.37_Loader_Keygen
+echo "已切换至工作目录"
+java -jar -Xmx1024m burp-loader-keygen.jar
