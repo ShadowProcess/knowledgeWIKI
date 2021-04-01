62bfac06279b160becd70a739fec498632d6d3d8
alex <jiahangsocket@163.com>
2021-04-01 13:13:42 +0800

    1.分布式事务和分布式id

---
+++ A-Test/src/main/java/com/example/DivideJava.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class DivideJava {
+    public static void main(String[] args) {
+        //java和C一样，这个 /  符号取小数点前的整数
+        //错了，计算当中 int / int 还是int类型。所以 35/4 =8; 8%3=2.
+        System.out.println(35/4%3);
+    }
+}
--- A-Test/src/main/java/com/example/Person.java
+++ A-Test/src/main/java/com/example/Person.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import static java.time.temporal.ChronoUnit.DAYS;
 
 public class Person {
+
     String name;
     String s;
 
