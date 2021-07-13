7f4630dd4a561003a42c1ea52c638955e5f477f9
alex <jiahangsocket@gmail.com>
2021-07-13 10:53:11 +0800

    1.Dependency Upgrade

--- A-TestWeb/src/main/java/com/example/controller/urlspecialsymbol/SpecialSymbolController.java
+++ A-TestWeb/src/main/java/com/example/controller/urlspecialsymbol/SpecialSymbolController.java
@@ -1,4 +1,4 @@
-package com.example.controller.specialsymbol;
+package com.example.controller.urlspecialsymbol;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
