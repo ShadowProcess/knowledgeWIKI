7063993278178e38d459bbb905eaaf7efe60cd6b
alex <jiahangsocket@gmail.com>
2021-07-16 15:07:05 +0800

    1.what is RPC?

---
+++ document-college/src/test/java/com/example/documentcollege/DocumentCollegeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.documentcollege;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentCollegeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
